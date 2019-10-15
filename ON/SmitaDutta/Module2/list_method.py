b40ea5e4931cc137d8c0ed4d25be308e1eb45cca
DuttaSmita <duttasmita@cityuniversity.edu>
2019-10-14 19:25:18 -0700

    Module 2 submission

---
+++ ON/SmitaDutta/Module2/list_method.py
@@ -0,0 +1,8 @@
+backpack=['book','laptop','pen','banana',2, True]
+#append() adds to the list
+backpack.append('keys')
+print(backpack)
+backpack.insert(0,'charger')
+print(backpack)
+backpack.remove('book')
+print(backpack)
