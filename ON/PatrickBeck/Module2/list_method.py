b13e93ebcd8c9a07f29045b97104dbe8de9e526a
PJB84 <patrickjbeck84@gmail.com>
2019-10-17 13:06:56 -0700

    Submission for Module 2

---
+++ ON/PatrickBeck/Module2/list_method.py
@@ -0,0 +1,13 @@
+backpack = ['book', 'laptop', 'pen', 'banana', 2, True]
+
+# insert() = adds to any index in the list
+backpack.insert(0, 'charger')
+
+# remove() = removes an item from any index in the list
+backpack.remove('book')
+
+# append() - adds to the end of the list
+backpack.append('keys')
+print(backpack)
+
+
