7997543f8ecd9a369332dc3553de0ec9bf7fecd7
lrschultz <lisa.r.schultz5122@gmail.com>
2019-10-19 12:46:50 -0700

    Submission for Module 2 - L.Schultz

---
+++ ON/LisaSchultz/Module2/list_method.py
@@ -0,0 +1,7 @@
+backpack = ['book', 'laptop', 'pen', 'banana', 2, True]
+
+#append () - adds to the end of the list
+backpack.append('keys')
+backpack.insert(0, 'charger')
+backpack.remove('book')
+print(backpack)
