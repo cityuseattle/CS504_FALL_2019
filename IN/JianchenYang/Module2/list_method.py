5ff24ad38319a6925a07e68d5728ec1cefd88d9a
Jianchen Yang <yang117yy@hotmail.com>
2019-10-23 16:20:11 -0700

    submission for Module 2

---
+++ IN/JianchenYang/Module2/list_method.py
@@ -0,0 +1,8 @@
+backpack = ['book','laptop','pen','banana',2,True]
+# append() - adds to the end of th list
+backpack.append('Keys')
+# insert() - adds to any index in the list
+backpack.insert(0, 'charger')
+# delete 'book'
+del backpack[1]
+print(backpack)
---
+++ IN/JianchenYang/Module2/my_func.py
@@ -0,0 +1,5 @@
+def my_func(name):
+    print('Wait. What!, ' + name + ' Just made this function ')
+
+
+my_func('Jianchen')
