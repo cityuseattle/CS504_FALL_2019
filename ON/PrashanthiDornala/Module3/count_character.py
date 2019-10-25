3b22a3e8d3d22d119b73fbace6a909f6cc2fa1b2
Prashanthi-User <dornalaprashanthi65@gmail.com>
2019-10-25 14:36:43 -0500

    Submission for Module 3

---
+++ ON/PrashanthiDornala/Module3/count_character.py
@@ -0,0 +1,13 @@
+import pprint
+
+message="""Peter piper picked a peck of pickled peppers
+           A peck of pickled peppers peter piper picked
+           If peter piper picked a peck of pickled peppers
+           Where's the peck of pickled peppers peter piper picked?"""
+
+count={}
+for character in message:
+    count.setdefault(character,0)
+    count[character]=count[character]+1
+
+pprint.pprint(count)
