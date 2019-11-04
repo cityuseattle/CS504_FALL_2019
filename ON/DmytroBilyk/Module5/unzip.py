b5a9163455b2ae111d6d11335c62992b8839349a
bilykdo <dmytro@cityuniversity.edu>
2019-11-04 11:42:43 -0800

    Submission for Module 5

---
+++ ON/DmytroBilyk/Module5/unzip.py
@@ -0,0 +1,12 @@
+import zipfile
+
+file = zipfile.ZipFile('Archive.zip')
+print(file.namelist())
+
+sampleInfo = file.getinfo('test0.py')
+print(sampleInfo)
+print(sampleInfo.file_size)
+print(sampleInfo.compress_size)
+
+file.extractall()
+file.close()
