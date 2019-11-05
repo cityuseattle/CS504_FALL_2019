e7302b14a8e1dd629c0b46f7d966f53e488e2307
szhUser <szh267270@hotmail.com>
2019-11-04 21:56:41 -0800

    Submission for Module5

---
+++ ON/ZhihanShi/Module5/delete.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+
+extension = input("What extension of file you want to delete?")
+for filename in os.listdir('./'):
+    print(filename)
+
+ans = input("Are you sure?(y/n)")
+if ans.lower()=='y':
+    for filename in os.listdir('./'):
+        if filename.endswith(extension):
+            os.unlink(filename)
---
+++ ON/ZhihanShi/Module5/discount.py
@@ -0,0 +1,7 @@
+sneakers = {'name':'Fire Jordan','price':800}
+
+def discount_price(product,discount):
+    newPrice = int(product['price']),"Discount price is lower than zero"
+    return newPrice
+print(discount_price(sneakers,0.5))
+print(discount_price(sneakers,2))
