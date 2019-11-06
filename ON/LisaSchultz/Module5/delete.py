2b6bbfbf66766abede5c85b8d9e4efcaf32b12a2
lrschultz <lisa.r.schultz5122@gmail.com>
2019-11-05 22:41:56 -0800

    Submission for Module 5 LSchultz

---
+++ ON/LisaSchultz/Module5/delete.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+
+extension = input("What extension of file do you want to delete?")
+# files under current directory
+for filename in os.listdir('./'):
+    if filename.endswith(extension):
+        #use print() to show what files will be deleted
+        #to avoid accidental deletion
+        print(filename)
+
+ans = input("Are you sure (y/n)? ")
+if ans.lower()=='y':
+    if filename.endswith(extension):
+        # this will permanently delete files
+        os.unlink(filename)
