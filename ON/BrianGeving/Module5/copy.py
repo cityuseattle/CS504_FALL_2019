34b1ccc9d06f79276a644d27da246a50a5095689
briang5 <briang5@uw.edu>
2019-11-10 13:12:43 -0800

    Submission for Module 5

---
+++ ON/BrianGeving/Module5/copy.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+shutil.copy('test0.txt', './Module5_Backup')
+shutil.copy('test0.py', './Module5_Backup')
+shutil.copy('test0.py', './Module5_Backup/test0_newName.txt')
+
+shutil.copy('test1.txt', './Module5_Backup')
+shutil.copy('test1.py', './Module5_Backup')
+shutil.copy('test2.py', './Module5_Backup/test2_newName.txt')
