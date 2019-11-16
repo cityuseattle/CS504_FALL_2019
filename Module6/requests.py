58846694979041b8889ecf2d16c58978bab703cc
Evan <evan@simplymeasured.com>
2019-11-16 14:42:21 -0800

    Submission for Module 6

---
+++ Module6/requests.py
@@ -0,0 +1,13 @@
+import requests
+
+res = requests.get('http://www.textfiles.com/computers/mrdos1.txt')
+
+try:
+    res.raise_for_status()
+    print("Status code: ", res.status_code)
+    print("Length of the text: ", len(res.text))
+    print(res.text[:100])
+
+except Exception as exc:
+    print("Oh no: %s" % (exc))
+
