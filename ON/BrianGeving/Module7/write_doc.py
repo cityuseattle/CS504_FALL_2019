ee96591b5c1beedd8239c25bda44c6ed17d0b632
briang5 <briang5@uw.edu>
2019-11-24 11:34:58 -0800

    Submission for Module 7

---
+++ ON/BrianGeving/Module7/write_doc.py
@@ -0,0 +1,16 @@
+import docx
+import requests
+
+res = requests.get('http://www.textfiles.com/computers/mrdos1.txt')
+res.raise_for_status
+text = res.text[:9310]
+doc = docx.Document()
+
+para1 = doc.add_paragraph('This is the first paragraph.', 'Title')
+for p in text.split('\n\n'):
+    try:
+        doc.add_paragraph(p)
+    except ValueError:
+        continue
+
+doc.save('firstWord.docx')
