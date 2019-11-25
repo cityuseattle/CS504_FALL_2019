1fd49f1e244f3156d3fc02432450ed8bee4d4ae2
szhUser <szh267270@hotmail.com>
2019-11-25 15:24:55 -0800

    Submission for Module8

---
+++ ON/ZhihanShi/Module8/schedule.py
@@ -0,0 +1,15 @@
+import time
+import datetime
+
+def schedule():
+    newyear = datetime.datetime(2020,1,1,0,0,0,)
+    while datetime.datetime.new()<newyear:
+        try:
+            print(datetime.datetime.new().strftime("%Y-%m-%d %H:%M:%S"), end='\r')
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+    else:
+        print("########## HAPPY NEW YEAR ##########")
+if __name__ == "__main__":
+    schedule()
