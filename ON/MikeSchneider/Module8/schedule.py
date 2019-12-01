bdcd8fdfc1e4d61520cc95c17e04a1e90a4d1d97
MikeSchneider <schneidermichael@cityuniversity.edu>
2019-12-01 11:05:34 -0800

    Submission for Module 8

---
+++ ON/MikeSchneider/Module8/schedule.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+
+
+def schedule():
+
+    newyear = datetime.datetime(2020, 1, 1, 0, 0, 0)
+    while datetime.datetime.now() < new year:
+        try:
+
+
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), end='\r')
+            
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+    else:
+        print("####### HAPPY NEW YEAR #######")
+
+
+if __name__ == "__main__":
+    schedule()
